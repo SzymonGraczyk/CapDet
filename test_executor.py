12f960f1f9651250b3488de013a20e0cef0043ee
sgraczyk <szymon.graczyk@intel.com>
2017-11-17 13:09:14 +0100

    Test2

--- test_executor.py
+++ test_executor.py
@@ -11,16 +11,22 @@ def execute_test(host_ip, test_script):
     channel.exchange_declare(exchange      = 'tests',
                              exchange_type = 'direct')
 
+#    callback_queue = 'tests_queue'
     res = channel.queue_declare(exclusive=False)
+    callback_queue = result.method.queue
+
+    print res
 
     corr_id = str(uuid.uuid4())
     channel.basic_publish(exchange='tests',
                           routing_key='1',
                           properties=pika.BasicProperties(
-                              reply_to = res.method.queue,
+                              reply_to = callback_queue,
                               correlation_id = corr_id,
                               content_type = 'application/json',
                           ),
                           body=str(body))
 
     connection.close()
+
+    print 'execute_test done'
